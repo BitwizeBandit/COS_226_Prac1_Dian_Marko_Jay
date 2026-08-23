6ac83873c4a1119aea1edc05e224d2699fce5d5e
Dian Le Roux <lerouxdian1@gmail.com>
2026-08-23 16:01:01 +0200

    Implemented the constructor for FilterLock

--- Code/practical_2/FilterLock.java
+++ Code/practical_2/FilterLock.java
@@ -1,12 +1,34 @@
 public class FilterLock implements Lock 
 {
+    // why we use 'volatile': volatile forces every read to go to main memory and every write to be flushed immediately, 
+    // so threads actually see each others updates in real time
 
-    private final int n;
-    private final VolatileInt[] level;
-    private final VolatileInt[] victim;
+    private final int n; // total num of threads using this lock, loops use it to check how many threads/levels needs to be checked
+    private final VolatileInt[] level; // waiting rooms, threads must traverse it before getting a lock
+                                      // if more than 1 thread tries to enter a level, then atleast 1 thread is blocked 
+    private final VolatileInt[] victim; // records which thread most recently declared itself willing to wait at level L
 
-    public FilterLock(int n) 
+    public FilterLock(int n) // constructor
     {
+        this.n = n; // num of levels
+
+        level = new VolatileInt[n]; // Each thread must pass through n−1 levels of “exclusion” to enter its CS
+        victim = new VolatileInt[n]; // 
+
+        // level[i] will track how far thread i has climbed, 0 means its not trying, higest level i wanna reach
+        // victim[L] will track which thread most recently offered to "lose" at level L, so its excludede from level L
+
+        // every thread starts at level 0
+        for (int i = 0; i < n; i++) 
+        {
+            level[i] = new VolatileInt(0);
+        }
+        
+       // every levels victim initialised, will get overwritten firt time any thread reaches the level
+        for (int L = 0; L < n; L++) 
+        {
+            victim[L] = new VolatileInt(0);
+        }
 
     }
 
